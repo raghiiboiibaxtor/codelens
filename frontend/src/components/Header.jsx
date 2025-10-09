ecd6f442078a812df7083bc52565ee9e40abb6b0
raghiiboiibaxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2025-10-09 19:03:57 +1300

    Safety

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 
-
 export default function Header() {
   return (
-    <header className="w-full pb-5 mb-10 flex flex-row items-center justify-between  border-b border-gray-100/10 text-white font-[Montserrat]">
-    
+    <header className="w-full pb-5 mb-10 flex flex-col md:flex-row sm:items-left lg:items-center justify-between gap-4 border-b border-gray-100/10 text-white font-[Montserrat]">
+      {/* Left: Logo + Title */}
       <div className="flex items-center gap-2">
-        <h1 className="text-3xl font-black ">CodeLens</h1>
-          <img
-            src="/CodelensLogo.png"
-            alt="Codelens logo"
-            className="h-8 w-8 object-contain"
+        <img
+          src="/CodelensLogo.png"
+          alt="Codelens logo"
+          className="h-8 w-8 object-contain"
         />
+        <h1 className="text-3xl font-black font-[Montserrat]">CodeLens</h1>
       </div>
 
-     
-      <div className="flex-1 text-center">
-        <p className="text-md font-light text-stone-200">
-        Meet your new AI powered Python Investigator 🕵️‍♂️ 🐍
+      {/* Center: Tagline */}
+      <div className="text-left">
+        <p className="text-sm lg:text-md font-medium text-stone-200">
+         Your AI-Powered Python Investigator 🕵️‍♂️ 🐍
         </p>
       </div>
 
-
+      {/* Right: GitHub Link */}
       <div>
         <a
           href="https://github.com/raghiiboiibaxtor"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-2xl font-black "
+          className="text-sm lg:text-md font-light lg:font-medium"
         >
-        <span className="underline">@rbxtr</span>
+          <span className="hover:underline">Built by: Raghiiboii Baxtor</span>
         </a>
       </div>
     </header>
